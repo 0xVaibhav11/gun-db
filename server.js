95333aca18d0726604f608cac63cd6a195c44d0b
Vaibhav Anand <vanoobcoder@gmail.com>
2022-11-15 21:11:02 +0530

    Deploy

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import Gun from "gun";
+
+const app = express();
+const port = process.env.PORT || 9000;
+app.use(cors());
+app.use(Gun.serve);
+
+app.get("/", (_, res) => {
+  res.status(200).send(`> DEBUG: RocketShare node is LIVE!`);
+});
+
+const server = app.listen(port, () => {
+  console.log(`> DEBUG: RocketShare node is listening on port ${port}`);
+});
+
+Gun({ web: server });
